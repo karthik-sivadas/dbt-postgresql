4ab7ade547881cb24d7e06b33ea28b3a705b35fd
Karthik Sivadas <karthiksivadas@outlook.com>
2024-05-06 09:34:46 +0000

    feat: update example_model.sql location

---
+++ models/stage/example_model.sql
@@ -0,0 +1,21 @@
+-- models/example_model.sql
+
+{{ config(materialized='table') }}
+
+WITH base AS (
+    SELECT
+        id,
+        user_name,
+        email,
+        sign_up_date,
+        last_login,
+        is_active,
+        score
+    FROM test_model
+)
+
+SELECT
+    COUNT(*) AS total_users,
+    AVG(score) AS average_score,
+    ROUND(100.0 * SUM(CASE WHEN is_active THEN 1 ELSE 0 END) / COUNT(*), 2) AS percentage_active_users
+FROM base
