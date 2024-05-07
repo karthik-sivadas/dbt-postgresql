5575042a4ce90e61e4cde21b6bec73262d074002
Karthik Sivadas <karthiksivadas@outlook.com>
2024-05-07 10:44:18 +0000

    fix: update filtered_users.sql

--- models/stage/filtered_users.sql
+++ models/stage/filtered_users.sql
@@ -5,6 +5,19 @@
 {% set is_active_filter = var('is_active_filter', None) %}
 {% set start_date = var('start_date', None) %}
 {% set end_date = var('end_date', None) %}
+{% set conditions = [] %}
+
+{% if is_active_filter != None %}
+  {% do conditions.append("is_active = " + is_active_filter | string) %}
+{% endif %}
+
+{% if start_date != None %}
+  {% do conditions.append("last_login >= '" + start_date + "'") %}
+{% endif %}
+
+{% if end_date != None %}
+  {% do conditions.append("last_login <= '" + end_date + "'") %}
+{% endif %}
 
 WITH base AS (
     SELECT
@@ -15,13 +28,10 @@ WITH base AS (
         last_login,
         is_active,
         score
-    FROM {{ source('my_database', 'test_model') }}
-    WHERE
-        ({% if is_active_filter != None %} is_active = {{ is_active_filter }}{% endif %})
-        {% if is_active_filter != None and (start_date != None or end_date != None) %} AND {% endif %}
-        ({% if start_date != None %} last_login >= '{{ start_date }}' {% endif %})
-        {% if start_date != None and end_date != None %} AND {% endif %}
-        ({% if end_date != None %} last_login <= '{{ end_date }}' {% endif %})
+    FROM test_model
+    {% if conditions %}
+    WHERE {{ conditions | join(' AND ') }}
+    {% endif %}
 )
 
 SELECT
